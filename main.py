661cfe9a3b317228320484bb1488f2fb4ab61e13
matthew-teetshorn <matthew.teetshorn@gmail.com>
2025-10-28 17:11:36 -0500

    added player class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -9,6 +10,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # Handle the window close event
@@ -18,7 +22,10 @@ def main():
         
         # Handle screen buffer
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
