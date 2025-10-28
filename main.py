590bd7e17da18de8c161c58f7d3b88367afbbac4
matthew-teetshorn <matthew.teetshorn@gmail.com>
2025-10-28 18:20:59 -0500

    added groups drawable and updatable

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pygame
 from constants import *
 from player import *
 
+# All objects which can be updated
+updatable = pygame.sprite.Group()
+# All objects which can be drawn
+drawable = pygame.sprite.Group()
+
+# Assign relevant groups to the player
+Player.containers = (updatable, drawable)
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,14 +28,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        # Update player position and rotation
-        player.update(dt)
+        # Update the updatables
+        updatable.update(dt)
         
         # Handle screen buffer
         screen.fill("black")
-        player.draw(screen)
+        # We must iterate over drawable to call overridden .draw() method
+        # as our 'hand drawn' player sprite does not have a .image attr
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
 
+        # update the clock (60 FPS)
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
